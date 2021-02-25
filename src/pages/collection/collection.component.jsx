c442fbc76f4f6988af9a8312b8b500a0e508bb81
KukushkinDmi1riy <777Dmitrij@gmail.com>
2021-02-25 10:32:30 +0300

    moved our directory and chop data into its own reducer, created respective selectors and update corresponding comments with redux flow for directory/shop

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { connect } from 'react-redux';
+
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import './collection.styles.scss'
+
+const CollectionPage = ({ collection }) => {
+    console.log(collection)
+    const {items, title} = collection;
+    return (
+        <div className="collection-page">
+            <h2>{title} </h2>
+            <div className="items">
+                {
+                    items.map(item => <CollectionItem key={item.id} item={item}/> )
+                }
+            </div>
+        </div>
+    );
+};
+
+
+const mapStateToPtops = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+
+export default connect(mapStateToPtops)(CollectionPage);
---
+++ src/redux/directory/directory.selectors.js
@@ -0,0 +1,9 @@
+import { createSelector } from 'reselect';
+
+const selectSections = state => state.directory;
+
+export const selectDirectorySections = createSelector(
+    [selectSections],
+    directory => directory.sections
+)
+
